21a6b89f39d05e4a1c1c273b811007db98b4e509
Avinash Pathrol <46990012+avinashpathrol1997@users.noreply.github.com>
2021-06-25 14:33:44 +0530

    fasf

---
+++ login/configuration/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  /*
+   * This file contains the configurations information of Twitter login app.
+   * It consists of Twitter app information, database information.
+   */
+
+  facebook_api_key: "895373264639392",
+  facebook_api_secret: "b43988e4f2477401bc105dc0025e4351",
+  callback_url: "http://localhost:3000/auth/facebook/callback",
+  use_database: false,
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: ""
+};
